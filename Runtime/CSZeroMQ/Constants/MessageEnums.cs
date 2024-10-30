b598a63b2e26f611cdaae5852be9e77020901ab4
Josef Fulem <fulemj@btlnet.com>
2024-10-30 16:12:34 +0100

    import and start

---
+++ Runtime/CSZeroMQ/Constants/MessageEnums.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CSZeroMQ.Constants
+{
+
+    /// <summary>
+    /// ZeroMQ message properties that return int.
+    /// </summary>
+    public enum MessageProperty : int
+    {
+        More = 1,
+        [Obsolete]
+        SourceFD = 2,
+        Shared = 3
+    }
+}
---
+++ Runtime/CSZeroMQ/ZMQException.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Runtime.InteropServices;
+using CSZeroMQ.Helpers;
+using CSZeroMQ.Native;
+
+namespace CSZeroMQ
+{
+
+    /// <summary>
+    /// Represents an error caused by ZeroMQ.
+    /// </summary>
+    public unsafe class ZMQException : ApplicationException
+    {
+        public ZMQException(int errno) : base(GetErrString(errno))
+        {
+            Errno = errno;
+        }
+
+        public ZMQException() : this(GetErrno()) { }
+
+        private static string GetErrString(int errno)
+        {
+            var errPtr = (IntPtr)ZMQ.zmq_strerror(errno);
+            return Marshal.PtrToStringAnsi(errPtr) ?? "Unknown error";
+        }
+
+        public int Errno { get; }
+
+        public static int GetErrno()
+        {
+            return ZMQ.zmq_errno();
+        }
+    }
+}
